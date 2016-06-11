26a37b5aac7786fd3d451a2e1d8256fd534807eb
Cathy <caathylee@gmail.com>
2016-06-11 10:40:53 -0700

    Rebase with current dev branch

--- src/client/components/Scoreboard.js
+++ src/client/components/Scoreboard.js
@@ -1,5 +1,4 @@
-import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
+import React from 'react';
 
 class Scoreboard extends React.Component {
   constructor(props) {
@@ -15,25 +14,25 @@ class Scoreboard extends React.Component {
         <h1 className="center-align">Scoreboard</h1>
         <ul className="collection score-collection">
           <li className="collection-item avatar">
-            <img className="user-icon" src="https://cdn0.iconfinder.com/data/icons/avatars-8/128/avatar-12-3-128.png"/>
+            <img alt="" className="user-icon" src="https://cdn0.iconfinder.com/data/icons/avatars-8/128/avatar-12-3-128.png" />
             <p className="user-stats"><span className="title"><p>1. Cathy</p></span>
               Score: 5,000,000
             </p>
           </li>
           <li className="collection-item avatar">
-            <img className="user-icon" src="https://cdn0.iconfinder.com/data/icons/avatars-8/128/avatar-33-2-128.png"/>
+            <img alt="" className="user-icon" src="https://cdn0.iconfinder.com/data/icons/avatars-8/128/avatar-33-2-128.png" />
             <p className="user-stats"><span className="title"><p>2. Jen</p></span>
               Score: 3,000,000
             </p>
           </li>
           <li className="collection-item avatar">
-            <img className="user-icon" src="https://cdn0.iconfinder.com/data/icons/avatars-8/128/avatar-52-2-128.png"/>
+            <img alt="" className="user-icon" src="https://cdn0.iconfinder.com/data/icons/avatars-8/128/avatar-52-2-128.png" />
             <p className="user-stats"><span className="title"><p>3. Ash</p></span>
                 Score: 1,000,000
             </p>
           </li>
           <li className="collection-item avatar">
-            <img className="user-icon" src="https://cdn0.iconfinder.com/data/icons/avatars-8/128/avatar-01-2-128.png"/>
+            <img alt="" className="user-icon" src="https://cdn0.iconfinder.com/data/icons/avatars-8/128/avatar-01-2-128.png" />
             <p className="user-stats"><span className="title"><p>4. David</p></span>
               Score: 0
             </p>
